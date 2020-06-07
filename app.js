c1e548b660a467397b1ff39ccbcb32f404047dcc
Hafsasohail18 <shafsa187@gmail.com>
2020-06-07 15:52:18 +0500

    fifth assignment

---
+++ app.js
@@ -0,0 +1,300 @@
+
+// CHAPTER: ALERTS
+
+/*alert("Assalam O Alaikum!");
+alert("Error! PLease enter a valid password.");
+alert("Welcome to JS World...\nHappy Coding!");
+alert("Welcome to JS World...");
+alert("Hello...I can run JS through my web browser's console.");
+
+ // CHAPTER: VARIABLES FOR STRINGS
+
+var username;
+var myName = "Hafsa Sohail";
+var message = "Hello World";
+alert(message);
+var studentName = "Jhone Doe";
+alert(studentName);
+var studentAge = "15 year old";
+alert(studentAge);
+var subject = "Certified Mobile Application Development";
+alert(subject);
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+var email;
+email = "shafsa187@gmail.com";
+alert(" the email address is " + email);
+var book;
+book = "The smarter way to learn Javascript";
+alert(" I am trying to learn from a book " + book);
+var text = "Yah! I can write HTML content through Javascript."; 
+document.write(text+"<br>"); 
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+  // CHAPTER: VARIABLES FOR NUMBER
+
+var age;
+age = "18 years old";
+alert(" I am " + age);
+var i = 14;
+alert(" You have visited this site " + i + " times. ");
+var birthYear;
+birthYear = "2001";
+document.write("My birth year is " + birthYear +  " Datatype of my declared variable is number."+"<br>");
+var visitorName;
+visitorName = "John Doe";
+var productTitle;
+productTitle = "T-shirt(s)";
+var quantity;
+quantity = 5;
+document.write(visitorName + " ordered " + quantity + productTitle + " on XYZ clothing store");*/
+
+
+   // CHAPTER: LEGAL AND ILLEGAL VARIABLES
+/*var name = "Hafsa", age = 18 , education = "Intermediate";
+ var = "userName","Symbolic_code","int_next","hello1","Hafsa";           //LEGAL VARIABLES
+ var = "1user",".hafsa","1stname","hello&bye","5disk"; */                  //ILLEGAL VARIABLES
+
+   // CHAPTER: MATH EXPRESSIONS
+/*var num1 = 203;
+var num2 = 786;
+var totalNum = num1+num2;
+document.write(" The sum of "+ num1 + " and " + num2 + " is " + totalNum + "<br>");
+var num1 = 203;
+var num2 = 786;
+var totalNum = num1-num2;
+document.write(" The subtraction of "+ num1 + " and " + num2 + " is " + totalNum + "<br>" );
+var num1 = 2;
+var num2 = 6;
+var totalNum = num1*num2;
+document.write(" The multiplication of "+ num1 + " and " + num2 + " is " + totalNum  + "<br>");
+var num1 = 2;
+var num2 = 6;
+var totalNum = num1/num2;
+document.write(" The division of "+ num1 + " and " + num2 + " is " + totalNum + "<br>" );
+var i = 22 ;
+document.write(" The value after variable declaration is: " + i  + "<br>" );
+let a = 5;
+document.write(" Initial value is: " + a  + "<br>");
+let b = ++a ;
+document.write(" The value after increment is: " + b + "<br>" );
+var c = 7 ;
+var d = b+c ;
+document.write(" Value after addition is: " + d + "<br>");
+let e = --d;
+document.write(" The value after decrement is: " + e + "<br>");
+var f = e/3;
+document.write(" The remainder is: " + f + "<br>");
+var ticPrice = 600;
+var num = 5 ;
+var cost = ticPrice*num ;
+document.write(" Total cost of " + num + " tickets " + " of a  movie " + cost + " PKR ");
+for( var y = 1; y <= 10 ; y++){
+    document.write("4" + "X" + y + "=" + 4*y + "<br>");
+}*/
+
+/*var C = 29 ;
+var F = (C * 9/5 )+32 ;
+document.write(C + "oC" + " is " + F + "oF" + "<br>");
+var f = 77;
+var c = (f-32)/1.8 ;
+document.write(f + "oF" + " is " + c + "oC" + "<br>" );*/
+
+/*document.write("<h1>Shopping Cart</h1>");
+var price1 = 650 ;
+document.write(" The price of item 1 is " + price1 + "<br>"  );
+var price2 = 100 ;
+document.write(" The price of item 2 is " + price2 + "<br>" );
+var quan1 = 3 ;
+document.write(" The quantity of item 1 is " + quan1 + "<br>");
+var quan2 = 7 ;
+document.write("The quantity of item 2 is " + quan2 + "<br>");
+var charges = 100 ;
+document.write(" Shipping Charges is " + charges + "<br>" );
+var amount = price1*quan1 + price2*quan2 +charges;
+document.write(" Total cost of your is " + amount);*/
+
+/*document.write("<h1>Marksheet</h1>");
+var t = 980 ;
+document.write(" Total marks: "+ t  + "<br>");
+var o = 804 ;
+document.write(" Obtained marks: "+ o+ "<br>" );
+var p = o*100/t;
+document.write(" Percentage : " + p + "%" + "<br>");*/
+
+/*document.write("<h1>Currency in PKR</h1>");
+var d = 10 ;
+var r = 25 ; 
+var tcurrency = 10*104.80+25*28;
+document.write(" Total Currency in PKR : " + tcurrency);*/
+
+/*var i = 22 ;
+var x = i+5*10/2 ;
+document.write(" Total Calculation: " + x );*/
+
+/*document.write("<h1>Age Calculator</h1>");
+var Cyear = 2020;
+document.write(" Current Year : " + Cyear + "<br>");
+var Byear = 2001;
+document.write(" Birth Year : " + Byear + "<br>" );
+var age = Cyear-Byear ;
+document.write(" Your Age : " + age + "<br>" );*/
+
+/*document.write("<h1>The Geometrizer</h1>");
+var r = 5 ;
+document.write(" The radius of circle : " + r + "<br>");
+var circum = 2*3.14*r ;
+document.write(" The circumference of circle : " + circum + "<br>");
+var area = 3.14*r*r;
+document.write(" The area of circle : " + area+ "<br>" );*/
+
+
+/*document.write("<h1>The Lifetime supply Calculator</h1>");
+var s = "tacos" ;
+document.write(" Favourite Snack : " + s + "<br>");
+var age = 18 ;
+document.write(" The current age : " + age + "<br>");
+var maxAge = 65 ;
+document.write(" The estimated maximum age : " + maxAge + "<br>" );
+var d  = 2 ;
+document.write(" The amount of snack per day : " + d + "<br>");
+var t = age*d;
+document.write(" You will need " + t + s + " to last  you until the ripe old age of " + maxAge + "<br>");*/
+
+
+
+ // CHAPTER: MATH EXPRESSIONS
+
+/*var a =prompt("Enter a number : ");
+document.write("<h2>Result</h2>");
+document.write(" The value of a is :" + a + "<br>" );
+
+document.write(" The value of ++a is :" + ++a + "<br>");
+document.write(" Now the value of a is :" + a + "<br>" );
+
+document.write(" The value of a++ is :" + a++ + "<br>" );
+document.write(" Now the value of a is :" + a + "<br>");
+
+document.write(" The value of --a is :" + --a + "<br>" );
+document.write(" Now the value of a is :" + a + "<br>");
+
+document.write(" The value of a-- is :" + a-- + "<br>" );
+document.write(" Now the value of a is :" + a + "<br>");*/
+
+/*var a= 2; b = 1;
+var r = --a - --b + ++b + b-- ;
+document.write(" a is : " + a + "<br>");
+document.write(" b is : " + b + "<br>" );
+document.write(" The result is : " + r + "<br>" );*/
+
+/*var Uname =prompt(" Enter your Name :");
+alert(" Hello " + Uname);*/
+
+/*var number = prompt(" Enter a Number :");
+for(let i = 1; i <= 10 ;i++){
+    document.write(number + "x" + i + "=" + number*i + "<br>");
+}*/
+
+/*var subject1 = prompt("Enter first subject : ");
+var subject2 = prompt("Enter second subject : ");
+var subject3 = prompt("Enter third subject : ");
+var Tmarks = 100 ;
+var Omarks1 = prompt(" Enter obtained marks for " + subject1);
+var Omarks2 = prompt(" Enter obtained marks for " + subject2);
+var Omarks3 = prompt(" Enter obtained marks for " + subject3);*/
+
+
+//CHAPTER: USER INPUT AND CONDITIONAL STATEMENT
+
+/*var x=prompt("Enter city name :");
+if(x === "Karachi"){
+    alert("Karachi is the city of lights.");
+}*/
+
+/*var g = prompt("Enter your gender:");
+        if(g === "Male"){
+        alert("Good Morning Sir!");
+        }
+        else{
+            alert("Good Morning Madam!");
+        }*/
+
+        /*var c = prompt("Enter colour of road traffic signal :");
+        if(c === "red"){
+        alert("Must Stop!");
+        }
+        else if (c === "yellow"){
+        alert("Ready to Move");
+        }
+        else{
+            alert("Move Now");
+        }*/
+
+        /*var f =prompt("Enter remaining fuel in car :");
+        if(f <= 0.25){
+            alert("Please refill the fuel in your car");
+        }*/
+
+   /* var a = 4;
+if (++a === 5){                                          //alert is displayed.
+alert("given condition for variable a is true");                
+}*/
+
+/*var b = 82;
+if (b++ === 83){
+alert("given condition for variable b is true");        //alert is not displayed.
+}*/
+
+/*var c = 12;
+if (c++ === 13){
+alert("condition 1 is true");
+}
+if (c === 13){
+alert("condition 2 is true");                              //alert is displayed.
+}
+if (++c < 14){
+alert("condition 3 is true");
+}
+if(c === 14){
+alert("condition 4 is true");
+}*/
+
+/*var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;                //alert is displayed.
+if (totalCost === laborCost + materialCost){
+alert("The cost equals");
+}*/
+
+
+/*var Omarks1 = +prompt(" Enter obtained marks in first subjects" );
+var Omarks2 = +prompt(" Enter obtained marks in second subject" );
+var Omarks3 = +prompt(" Enter obtained marks in third subject" );
+var Tmarks1 = +prompt("Enter total marks of first subject :");
+var Tmarks2 = +prompt("Enter total marks of second subject : ");
+var Tmarks3 = +prompt("Enter total marks of  third subject : ");
+var totalO= Omarks1+Omarks2+Omarks3;
+document.write(" Obtained marks : " + totalO + "<br>" );
+var totalT= Tmarks1+Tmarks2+Tmarks3;
+document.write(" Total Marks : " + totalT + "<br>" );
+var per = totalO*100/totalT ;
+document.write(" Percentage : " + per + "%" + "<br>");
+if(per <= 80){
+    document.write(" Grade : " + " A-one ");
+}
+else if (per <= 70) {
+    document.write(" Grade : " + "A");
+} 
+else if (per <= 60) {
+    document.write(" Grade : " + "B");
+}
+else{
+    document.write(" Grade : " + "F");
+}*/
+
+
+             
+
+
+
+
+
